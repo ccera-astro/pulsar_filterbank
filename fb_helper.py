8065c638ea08117383fec51b28b737a242bcbd72
Marcus Leech <mleech@ripnet.com>
2018-12-06 14:32:28 -0500

    Had the helper code write out the header file

--- fb_helper.py
+++ fb_helper.py
@@ -1,5 +1,5 @@
 # this module will be imported in the into your flowgraph
-
+import time
 def determine_rate(srate, fbsize):
     bankrate = float(srate)/float(fbsize)
     for i in range(2500,17000):
@@ -7,4 +7,18 @@ def determine_rate(srate, fbsize):
         irate = float(int(frate))
         if (frate == irate):
             return int(bankrate/irate)
-    raise ValueError("No suitable filterbank sample rate could be determined--try different FB size")
+    raise ValueError("No suitable filterbank sample rate could be determined--try different FB size or sample rate")
+
+def write_header(fn, freq, bw, fbsize, fbrate):
+    f = open(fn, "w")
+    ltp = time.gmtime(time.time())
+    f.write("frequency=%.5f\n" % freq)
+    f.write("RF sample rate=%.5f\n" % bw)
+    f.write("Filterbank Size=%d\n" % fbsize)
+    f.write("Filterbank output rate=%d\n" % fbrate)
+    f.write("Approx start UTC Date=%04d%02d%02d\n" % (ltp.tm_year,
+        ltp.tm_mon, ltp.tm_mday))
+    f.write("Approx start UTC Time=%02d:%02d:%02d\n" % (ltp.tm_hour,
+        ltp.tm_min, ltp.tm_sec))
+    
+
