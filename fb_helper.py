1bb77d4c584a99cc7e7a0d5aec6d8f7bf437eb2a
Marcus Leech <mleech@ripnet.com>
2018-12-14 02:11:58 -0500

    Had it write expected disk write rate to ASCII header info.

--- fb_helper.py
+++ fb_helper.py
@@ -53,6 +53,7 @@ def write_header(fn, freq, bw, fbsize, fbrate):
         ltp.tm_mon, ltp.tm_mday))
     f.write("Approx start UTC Time=%02d:%02d:%02d\n" % (ltp.tm_hour,
         ltp.tm_min, ltp.tm_sec))
+    f.write("Expected disk write rate: %6.2f mbyte/sec\n" % ((fbsize*fbrate*2.0)/1.0e6))
     
 def synched(startt,dummy):
     
