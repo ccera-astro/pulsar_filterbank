89ad0329d4ac7b47e20b382e396aba18ef412381
Marcus Leech <mleech@ripnet.com>
2020-08-27 20:48:04 -0400

    Updates to PSR database file to include P0 Have observe pulsar pick P0 out of database, pass it on. Make HP-corner be dependent on 1/.0/P0

--- makepsr_db.py
+++ makepsr_db.py
@@ -2,13 +2,13 @@
 import os
 import sys
 def decimalize(s):
-	stoks=s.split(":")
-	hours = float(stoks[0])
-	minutes = float(stoks[1])
-	seconds = float(stoks[1])
-	sign = -1.0 if hours < 0.0 else 1
-	hours = abs(hours)
-	return (sign*(hours+(minutes/60.0)+(seconds/3600.0)))
+    stoks=s.split(":")
+    hours = float(stoks[0])
+    minutes = float(stoks[1])
+    seconds = float(stoks[1])
+    sign = -1.0 if hours < 0.0 else 1
+    hours = abs(hours)
+    return (sign*(hours+(minutes/60.0)+(seconds/3600.0)))
 
 fp = sys.stdin
 
@@ -17,14 +17,19 @@ lines=fp.readlines()
 for line in lines:
     line = line.replace("\n", "")
     toks = line.split()
-    if len(toks) == 12 and unicode(toks[0]).isnumeric() == True:
+    if "---" not in line and len(toks) >= 7 and unicode(toks[0]).isnumeric() == True:
        bname=toks[1].lower()
        jname=toks[2].lower()
+       if (jname == "*" or bname == "*"):
+           continue
        ra=decimalize(toks[3])
        dec=decimalize(toks[4])
-       dm=toks[7]
-       pw50=float(toks[8])*1.0e-3
-       fmt="NAME=%s; DEC=%-7.3f; RA=%-7.3f; PW50=%-9.6f; DM=%s"
-       print fmt % (bname, dec, ra, pw50, dm)
+       dm=toks[5]
+       if (toks[6] == "*"):
+           continue
+       pw50=float(toks[6])*1.0e-3
+       p0=float(toks[7])
+       fmt="NAME=%s; DEC=%-7.3f; RA=%-7.3f; PW50=%-9.6f; DM=%s; P0=%-9.6f"
+       print fmt % (bname, dec, ra, pw50, dm, p0)
        if (bname != jname):
-		   print fmt % (jname, dec, ra, pw50, dm)
+           print fmt % (jname, dec, ra, pw50, dm, p0)
